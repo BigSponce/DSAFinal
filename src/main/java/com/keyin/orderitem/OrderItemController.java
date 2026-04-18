50a9c3b1444004fee5eab8ea3ff870298f4d083f
BigSponce <noseworthyspencer@gmail.com>
2026-04-18 16:56:31 -0230

    Delete src/main directory

--- src/main/java/com/keyin/orderitem/OrderItemController.java
+++
@@ -1,7 +0,0 @@
-package com.keyin.orderitem;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class OrderItemController {
-}
--- src/main/java/com/keyin/orderitem/OrderItemRepository.java
+++
@@ -1,8 +0,0 @@
-package com.keyin.orderitem;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderItemRepository extends CrudRepository<OrderItem, Long> {
-}
